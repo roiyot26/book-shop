13ca0b1a6f66ebacbad485393793f87f57433bc8
roi yotvat <roiyot261098@gmail.com>
2025-01-09 13:33:21 +0200

    add book modal

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,34 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
+ 
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path, notice: 'Book added successfully.'
+    else
+      @books = Book.all
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  # private
+  def toggle_availability
+      @book = Book.find(params[:id])
+      puts "book, #{@book.id}"
+      @book.update!(is_available: !@book.is_available)
+      puts "book, #{@book.id}"
+    
+
+      redirect_to books_path, notice: 'Book status updated successfully.'
+  end    
+
+  def book_params
+    params.require(:book).permit(:name, :author, :rent_price, :is_available,:image_url)
+  end
+
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,18 @@
 class Book < ApplicationRecord
+  before_save :titleize_name_and_author
+  before_save :set_default_image_url, if: -> { image_url.blank? }
+  validates :rent_price, numericality: { greater_than_or_equal_to: 0 }
+
+  # private
+
+  def set_default_image_url
+    self.image_url ||= 'assets/images/hp7.png'
+  end
+
+  private
+
+  def titleize_name_and_author
+    self.name = name.titleize
+    self.author = author.titleize
+  end
 end
